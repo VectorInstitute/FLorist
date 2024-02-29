fb996a03c156369289935f0567194710d61f00c3
Marcelo Lotif <marcelo.lotif@vectorinstitute.ai>
2024-02-29 16:35:06 -0500

    Fixing pre commits

--- florist/api/monitoring/metrics.py
+++ florist/api/monitoring/metrics.py
@@ -1,3 +1,4 @@
+"""Classes for the instrumentation of metrics reporting from clients and servers."""
 import json
 from logging import DEBUG
 from typing import Any, Dict, Optional
@@ -7,10 +8,8 @@ from fl4health.reporting.metrics import DateTimeEncoder, MetricsReporter
 from flwr.common.logger import log
 
 
-class RedisMetricsReporter(MetricsReporter):
-    """
-    Saves the metrics to a Redis instance while it records them.
-    """
+class RedisMetricsReporter(MetricsReporter):  # type: ignore
+    """Save the metrics to a Redis instance while it records them."""
 
     def __init__(
         self,
@@ -18,6 +17,8 @@ class RedisMetricsReporter(MetricsReporter):
         run_id: Optional[str] = None,
     ):
         """
+        Init an instance of RedisMetricsReporter.
+
         :param redis_connection: (redis.client.Redis) the connection object to a Redis. Should be the output
             of redis.Redis(host=host, port=port)
         :param run_id: (Optional[str]) the identifier for the run which these metrics are from.
@@ -28,8 +29,9 @@ class RedisMetricsReporter(MetricsReporter):
 
     def add_to_metrics(self, data: Dict[str, Any]) -> None:
         """
-        Adds a dictionary of data into the main metrics dictionary. At the end, dumps the current
-        state of the metrics to Redis.
+        Add a dictionary of data into the main metrics dictionary.
+
+        At the end, dumps the current state of the metrics to Redis.
 
         :param data: (Dict[str, Any]) Data to be added to the metrics dictionary via .update().
         """
@@ -38,8 +40,9 @@ class RedisMetricsReporter(MetricsReporter):
 
     def add_to_metrics_at_round(self, fl_round: int, data: Dict[str, Any]) -> None:
         """
-        Adds a dictionary of data into the metrics dictionary for a specific FL round. At the end,
-        dumps the current state of the metrics to Redis.
+        Add a dictionary of data into the metrics dictionary for a specific FL round.
+
+        At the end, dumps the current state of the metrics to Redis.
 
         :param fl_round: (int) the FL round these metrics are from.
         :param data: (Dict[str, Any]) Data to be added to the round's metrics dictionary via .update().
@@ -48,9 +51,7 @@ class RedisMetricsReporter(MetricsReporter):
         self.dump()
 
     def dump(self) -> None:
-        """
-        Dumps the current metrics to Redis under the run_id name.
-        """
+        """Dump the current metrics to Redis under the run_id name."""
         encoded_metrics = json.dumps(self.metrics, cls=DateTimeEncoder)
         log(DEBUG, f"Dumping metrics to redis at key '{self.run_id}': {encoded_metrics}")
         self.redis_connection.set(self.run_id, encoded_metrics)
