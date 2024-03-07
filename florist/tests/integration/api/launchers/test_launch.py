e1dc190e27ddca5098306f76565264d49e9857c3
Marcelo Lotif <marcelo.lotif@vectorinstitute.ai>
2024-03-07 11:00:05 -0500

    Adding client integration test with redis

--- florist/tests/integration/api/launchers/test_launch.py
+++ florist/tests/integration/api/launchers/test_launch.py
@@ -1,38 +1,16 @@
 import os
 import re
 import tempfile
-from functools import partial
 from pathlib import Path
-from typing import Callable, Dict
 
 import torch
-from fl4health.server.base_server import FlServer
 
 from florist.api.launchers.local import launch
-from florist.api.clients.mnist import MnistClient, MnistNet
-from florist.tests.utils.api.fl4health_utils import get_server_fedavg
+from florist.api.clients.mnist import MnistClient
+from florist.tests.utils.api.launch_utils import get_server
 
 
-def fit_config(batch_size: int, local_epochs: int, current_server_round: int) -> Dict[str, int]:
-    return {
-        "batch_size": batch_size,
-        "current_server_round": current_server_round,
-        "local_epochs": local_epochs,
-    }
-
-
-def get_server(
-    fit_config: Callable[..., Dict[str, int]] = fit_config,
-    n_clients: int = 2,
-    batch_size: int = 8,
-    local_epochs: int = 1,
-) -> FlServer:
-    fit_config_fn = partial(fit_config, batch_size, local_epochs)
-    server = get_server_fedavg(model=MnistNet(), n_clients=n_clients, fit_config_fn=fit_config_fn)
-    return server
-
-
-def assert_string_in_file(file_path: str, search_string: str) -> bool:
+def find_string_in_file(file_path: str, search_string: str) -> bool:
     with open(file_path, "r") as f:
         file_contents = f.read()
         match = re.search(search_string, file_contents)
@@ -61,4 +39,4 @@ def test_launch() -> None:
             client_base_path,
         )
 
-        assert_string_in_file(f"{server_path}.out", "FL finished in")
+        assert find_string_in_file(f"{server_path}.out", "FL finished in")
