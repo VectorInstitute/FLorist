d7b430826448872f9fc878a54285407681fd0d58
Marcelo Lotif <marcelo.lotif@vectorinstitute.ai>
2024-04-09 17:08:31 -0400

    Adding more information to the Job

--- florist/api/db/entities.py
+++ florist/api/db/entities.py
@@ -1,23 +1,63 @@
 """Definitions for the MongoDB database entities."""
 import uuid
-from typing import Optional
+from enum import Enum
+from typing import List, Optional
 
 from pydantic import BaseModel, Field
 from typing_extensions import Annotated
 
+from florist.api.clients.common import Client
 from florist.api.servers.common import Model
 
 
 JOB_DATABASE_NAME = "job"
 
 
+class JobStatus(Enum):
+    """Enumeration of all possible statuses of a Job."""
+
+    NOT_STARTED = "NOT_STARTED"
+    IN_PROGRESS = "IN_PROGRESS"
+    FINISHED_WITH_ERROR = "FINISHED_WITH_ERROR"
+    FINISHED_SUCCESSFULLY = "FINISHED_SUCCESSFULLY"
+
+
+class ClientInfo(BaseModel):
+    """Define the information of an FL client."""
+
+    id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    client: Client = Field(...)
+    service_address: str = Field(...)
+    data_path: str = Field(...)
+    redis_host: str = Field(...)
+    redis_port: str = Field(...)
+
+    class Config:
+        """MongoDB config for the ClientInfo DB entity."""
+
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "client": "MNIST",
+                "service_address": "locahost:8081",
+                "data_path": "path/to/data",
+                "redis_host": "localhost",
+                "redis_port": "6880",
+            },
+        }
+
+
 class Job(BaseModel):
     """Define the Job DB entity."""
 
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    status: JobStatus = Field(default=JobStatus.NOT_STARTED)
     model: Optional[Annotated[Model, Field(...)]]
+    server_address: Optional[Annotated[str, Field(...)]]
+    server_info: Optional[Annotated[str, Field(...)]]
     redis_host: Optional[Annotated[str, Field(...)]]
     redis_port: Optional[Annotated[str, Field(...)]]
+    clients_info: Optional[Annotated[List[ClientInfo], Field(...)]]
 
     class Config:
         """MongoDB config for the Job DB entity."""
@@ -26,8 +66,20 @@ class Job(BaseModel):
         schema_extra = {
             "example": {
                 "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
+                "status": "NOT_STARTED",
                 "model": "MNIST",
-                "redis_host": "locahost",
+                "server_address": "localhost:8080",
+                "server_info": '{"n_server_rounds": 3, "batch_size": 8}',
+                "redis_host": "localhost",
                 "redis_port": "6879",
+                "client_info": [
+                    {
+                        "client": "MNIST",
+                        "service_address": "locahost:8081",
+                        "data_path": "path/to/data",
+                        "redis_host": "localhost",
+                        "redis_port": "6880",
+                    },
+                ],
             },
         }
--- florist/api/routes/server/job.py
+++ florist/api/routes/server/job.py
@@ -1,7 +1,8 @@
-"""The /job FastAPI routes."""
+"""FastAPI routes for the job."""
+import json
 from typing import Any, Dict
 
-from fastapi import APIRouter, Body, Request, status
+from fastapi import APIRouter, Body, HTTPException, Request, status
 from fastapi.encoders import jsonable_encoder
 
 from florist.api.db.entities import JOB_DATABASE_NAME, Job
@@ -26,9 +27,20 @@ def new_job(request: Request, job: Job = Body(...)) -> Dict[str, Any]:  # noqa: 
     :param request: (fastapi.Request) the FastAPI request object.
     :param job: (Job) The Job instance to be saved in the database.
     :return: (Dict[str, Any]) A dictionary with the attributes of the new Job instance as saved in the database.
+    :raises: (HTTPException) a 400 if job.server_info is not None and cannot be parsed into JSON.
     """
-    job = jsonable_encoder(job)
-    result = request.app.database[JOB_DATABASE_NAME].insert_one(job)
+    if job.server_info is not None:
+        try:
+            json.loads(job.server_info)
+        except json.JSONDecodeError as e:
+            error_message = (
+                "job.server_info could not be parsed into JSON. " f"job.server_info: {job.server_info}. Error: {e}"
+            )
+            raise HTTPException(status_code=400, detail=error_message) from e
+
+    json_job = jsonable_encoder(job)
+
+    result = request.app.database[JOB_DATABASE_NAME].insert_one(json_job)
     created_job = request.app.database[JOB_DATABASE_NAME].find_one({"_id": result.inserted_id})
 
     assert isinstance(created_job, dict)
