4f6ba30b938ae219c3f4908f295a0b56ba82771c
Marcelo Lotif <marcelo.lotif@vectorinstitute.ai>
2024-04-25 17:34:12 -0400

    Adding prettier

--- florist/app/page.tsx
+++ florist/app/page.tsx
@@ -1,7 +1,5 @@
 import { ReactElement } from "react/React";
 
 export default function Home(): ReactElement {
-    return (
-        <span>Content goes here</span>
-    );
+  return <span>Content goes here</span>;
 }
