6e4ade55057eaf3da0132fe07c9bbafaa8620712
Marcelo Lotif <lotif@users.noreply.github.com>
2024-05-21 14:05:48 -0400

    Making some changes in order to make the material dashboard template fully work (#42)

--- florist/app/layout.tsx
+++ florist/app/layout.tsx
@@ -39,20 +39,18 @@ export default function RootLayout({
                 <script
                     dangerouslySetInnerHTML={{
                         __html: `
-                            <!--
-                            =========================================================
-                            * Material Dashboard 2 - v3.0.0
-                            =========================================================
-
-                            * Product Page: https://www.creative-tim.com/product/material-dashboard
-                            * Copyright 2021 Creative Tim (https://www.creative-tim.com)
-                            * Licensed under MIT (https://www.creative-tim.com/license)
-                            * Coded by Creative Tim
-
-                            =========================================================
-
-                            * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-                            -->
+//                             =========================================================
+//                             * Material Dashboard 2 - v3.0.0
+//                             =========================================================
+//
+//                             * Product Page: https://www.creative-tim.com/product/material-dashboard
+//                             * Copyright 2021 Creative Tim (https://www.creative-tim.com)
+//                             * Licensed under MIT (https://www.creative-tim.com/license)
+//                             * Coded by Creative Tim
+//
+//                             =========================================================
+//
+//                             * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
                 `,
                     }}
                 />
